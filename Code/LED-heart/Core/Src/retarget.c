ac25f5e6b18ad9415312f3295170b0209d11a224
emaneric <eric.n.tech@gmail.com>
2026-07-29 00:03:30 +0100

    Changed optiisation, removed unused code, fixed bug of wrong flash size for KU4 chip

--- Code/LED-heart/Core/Src/retarget.c
+++
@@ -1,6 +0,0 @@
-#include "SEGGER_RTT.h"
-
-int __io_putchar(int ch) {
-    SEGGER_RTT_PutChar(0, (char)ch);
-    return ch;
-}
--- Code/LED-heart/Drivers/SEGGER_RTT/SEGGER_RTT_Conf.h
+++
@@ -1,1 +0,0 @@
-// Project-specific RTT overrides. See SEGGER_RTT_ConfDefaults.h for all options.
